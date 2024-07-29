a9f6b0ca8497661e09e316d9cf80be8935e567a3
ujjwal <ujjwalsonawatjps7@gmail.com>
2024-07-29 15:07:44 +0530

    done

---
+++ script.js
@@ -0,0 +1,5 @@
+// var crsr = document.querySelector(".crsr");
+// document.addEventListener("mousemove", function (dets) {
+//     crsr.style.left = dets.x + "10px"
+//     crsr.style.top = dets.y + "10px"
+// });
