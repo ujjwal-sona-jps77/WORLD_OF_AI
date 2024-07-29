7bbfbf1b4175439c19aea9d70ccc0a854c49197f
Ujjwal Sonawat <ujjwalsonawatjps7@gmail.com>
2024-07-29 19:06:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-// var crsr = document.querySelector(".pc .crsr");
-// window.addEventListener("mousemove", function (dets) {
-//     crsr.style.left = dets.x + "10px",
-//     crsr.style.top = dets.y + "10px"
-// });
+var crsr = document.querySelector(".pc .crsr");
 
-  circle.style.backgroundColor = colors[index % colors.length];
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
 });
 
-window.addEventListener("mousemove", function(e){
-  coords.x = e.clientX;
-  coords.y = e.clientY;
-  
-});
-
-function animateCircles() {
-  
-  let x = coords.x;
-  let y = coords.y;
-  
-  circles.forEach(function (circle, index) {
-    circle.style.left = x - 12 + "px";
-    circle.style.top = y - 12 + "px";
-    
-    circle.style.scale = (circles.length - index) / circles.length;
-    
-    circle.x = x;
-    circle.y = y;
-
-    const nextCircle = circles[index + 1] || circles[0];
-    x += (nextCircle.x - x) * 0.3;
-    y += (nextCircle.y - y) * 0.3;
-  });
  
-  requestAnimationFrame(animateCircles);
-}
 
-animateCircles();
 
