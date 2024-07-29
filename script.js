a62f925f68e6285f132df93aa8c04d00e0321d71
Ujjwal Sonawat <ujjwalsonawatjps7@gmail.com>
2024-07-29 19:04:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// var crsr = document.querySelector(".crsr");
-// document.addEventListener("mousemove", function (dets) {
-//     crsr.style.left = dets.x + "10px"
+// var crsr = document.querySelector(".pc .crsr");
+// window.addEventListener("mousemove", function (dets) {
+//     crsr.style.left = dets.x + "10px",
 //     crsr.style.top = dets.y + "10px"
 // });
 
