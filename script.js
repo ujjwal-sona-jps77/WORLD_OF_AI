f12d1edcc03f41892f1ca69f99ef16d0af933a33
Ujjwal Sonawat <ujjwalsonawatjps7@gmail.com>
2024-07-30 13:04:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var crsr = document.querySelector(".pc .crsr");
 
 document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + "px";
-  crsr.style.top = dets.y + "px";
+  crsr.style.left = dets.x + 25 + "px";
+  crsr.style.top = dets.y + 25 + "px";
 });
 
- 
-
-
+change = document.querySelector(".pc .page6 .down .change");
+change.addEventListener("mouseover" , function(){
+    crsr.style.width = `3vw`
+    crsr.style.height = `5vh`
+    crsr.style.backgroundColor = "#white"
+})
+change.addEventListener("mouseleave" , function(){
+    crsr.style.width = `20px`
+    crsr.style.height = `20px`
+    crsr.style.backgroundColor = `#edbfff`
+})
