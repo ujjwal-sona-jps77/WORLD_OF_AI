39e8cb638c392cb1768a707ed60e3caf58274e4b
ujjwal <ujjwalsonawatjps7@gmail.com>
2024-07-30 15:09:57 +0530

    done

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
 var crsr = document.querySelector(".pc .crsr");
 
-document.addEventListener("mousemove", function (dets) {
-  crsr.style.left = dets.x + 25 + "px";
-  crsr.style.top = dets.y + 25 + "px";
-});
+function mousefollower() {
+  document.addEventListener("mousemove", function (dets) {
+    crsr.style.left = dets.x + 25 + "px";
+    crsr.style.top = dets.y + 25 + "px";
+  });
+}
 
 change = document.querySelector(".pc .page6 .down .change");
-change.addEventListener("mouseover" , function(){
-    crsr.style.width = `3vw`
-    crsr.style.height = `5vh`
-    crsr.style.backgroundColor = "#white"
-<<<<<<< HEAD
+change.addEventListener("mouseover", function () {
+  crsr.style.width = `3vw`;
+  crsr.style.height = `5vh`;
+  crsr.style.backgroundColor = "#white";
 });
-=======
-})
->>>>>>> 0a3925d6e85c89ec04607829aa6963d7d1869fb7
-change.addEventListener("mouseleave" , function(){
-    crsr.style.width = `20px`
-    crsr.style.height = `20px`
-    crsr.style.backgroundColor = `#edbfff`
-<<<<<<< HEAD
-});;
-=======
-})
->>>>>>> 0a3925d6e85c89ec04607829aa6963d7d1869fb7
+change.addEventListener("mouseleave", function () {
+  crsr.style.width = `20px`;
+  crsr.style.height = `20px`;
+  crsr.style.backgroundColor = `#edbfff`;
+});
+menu_in = document.querySelector(".pc .menu_in");
+mi = document.querySelector(".pc nav .menu i");
+
+function menu_animation() {
+  close_pc = document.querySelector(".pc nav .menu #close");
+  mi.addEventListener("click", function () {
+    menu_in.style.right = `0`;
+    menu_in.style.opacity = 1;
+    mi.style.display = "none";
+    close_pc.style.display = "block";
+    close_pc.style.display = "block";
+  });
+
+  close_pc.addEventListener("click", function () {
+    close_pc.style.display = "none";
+    mi.style.display = "block";
+    menu_in.style.right = `-100%`;
+  });
+}
+
+var menu_m_open = document.querySelector(".mobile .main .nav #open")
+var menu_m_close = document.querySelector(".mobile .main .nav #close")
+var menu_m = document.querySelector(".mobile .main .menu_in_m")
+
+
+function menu_mobile(){
+  menu_m_open.addEventListener("click" , function(){
+    menu_m.style.right = `0`
+    menu_m.style.opacity = 1
+    menu_m_open.style.display = "none"
+    menu_m_close.style.display = "block"
+  });
+
+  menu_m_close.addEventListener("click" , function(){
+    menu_m.style.right = `-100%`;
+    menu_m.style.opacity = 0;
+    menu_m_close.style.display = "none";
+    menu_m_open.style.display = "block"
+  })
+}
+
+menu_animation();
+mousefollower();
+menu_mobile();
